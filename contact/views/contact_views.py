ff49282d2e9ee4be62f4fa8b2fe02d1e026e697a
GuilhermeSaito <hguilherme433@gmail.com>
2024-04-24 02:40:34 -0400

    csrfToken, widgets, forms, Clean, ValidationError

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contacts=Contact.objects.filter(show=True).order_by('-id')[10:20]
+    contacts=Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')
+    paginator=Paginator(contacts, 10)
+    page_number=request.GET.get("page")
+    page_obj=paginator.get_page(page_number)
 
-    context={'contacts':contacts,
+    context={'page_obj':page_obj,
              'site_tiles':'Contatos - '
              }
 
@@ -29,9 +35,14 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
+    
+    paginator=Paginator(contacts, 10)
+    page_number=request.GET.get("page")
+    page_obj=paginator.get_page(page_number)
 
-    context={'contacts':contacts,
-             'site_tiles':'Search - '
+    context={'page_obj':page_obj,
+             'site_tiles':'Search - ',
+             'search_value':search_value,
              }
 
     return render(request,
